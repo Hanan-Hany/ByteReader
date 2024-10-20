41fe59e3c1252a874d22559758a74e8e67b231ce
hp <hp@DESKTOP-PT3GOGR>
2024-10-20 23:52:29 +0300

    Ecommerce website .Net Core MVC

---
+++ Luky Web/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using Luky_Web.Data;
+using Luky_Web.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Luky_Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicaionDbContext _context;
+        public CategoryController(ApplicaionDbContext context, IWebHostEnvironment webHostEnvironment)
+        {
+            _context = context;
+           
+        }
+        List<Category> categories;
+        public IActionResult Index()
+        {
+           categories =_context.Categories.ToList();
+
+            return View(categories);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Category category)
+        {
+
+           _context.Categories.Add(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+    }
+}
---
+++ Luky Web/Data/ApplicaionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Luky_Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Luky_Web.Data
+{
+    public class ApplicaionDbContext: DbContext
+    {
+        public ApplicaionDbContext(DbContextOptions <ApplicaionDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
