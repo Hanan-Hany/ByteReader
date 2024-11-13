ba30f5ce73eda7ec8f9aa4922a4857bbca3235e4
nour hany <hananhanyfathy@gmail.com>
2024-11-13 17:17:34 +0200

    Add Edit Category View

--- Luky Web/Controllers/CategoryController.cs
+++ Luky Web/Controllers/CategoryController.cs
@@ -42,5 +42,33 @@ namespace Luky_Web.Controllers
             return View();
         }
 
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+           
+            if (ModelState.IsValid)
+            {
+                _context.Categories.Update(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
     }
 }
