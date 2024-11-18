4f6bea022aea2ae5064c8947379320cec875495f
nour hany <hananhanyfathy@gmail.com>
2024-11-18 03:13:17 +0200

    Add delete method in Category Controller

--- Luky Web/Controllers/CategoryController.cs
+++ Luky Web/Controllers/CategoryController.cs
@@ -70,5 +70,36 @@ namespace Luky_Web.Controllers
             return View();
         }
 
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Category? category = _context.Categories.Find(id);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+
+        }
+        [HttpPost,ActionName("Delete")]
+        public IActionResult DeletePost( int ?id)
+        {
+            Category? category = _context.Categories.Find(id);
+            if (category == null) 
+            { return NotFound();
+            }
+           
+            
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            
+            
+        }
+
     }
 }
