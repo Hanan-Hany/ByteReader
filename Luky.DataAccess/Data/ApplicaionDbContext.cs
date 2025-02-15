cbdae84de87255dda68dd08f21564963a56246aa
nour hany <hananhanyfathy@gmail.com>
2025-02-15 06:38:48 +0200

    Apply N-Tier Architecture

---
+++ Luky.DataAccess/Data/ApplicaionDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Luky.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Luky.DataAccess.Data
+{
+    public class ApplicaionDbContext: DbContext
+    {
+        public ApplicaionDbContext(DbContextOptions <ApplicaionDbContext> options):base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
