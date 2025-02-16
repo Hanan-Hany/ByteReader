41b35b45598cf0baec4fc6910d6de6b0844de8f0
nour hany <hananhanyfathy@gmail.com>
2025-02-16 22:04:28 +0200

    General Reopsitory

---
+++ Luky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using Luky.DataAccess.Data;
+using Luky.DataAccess.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicaionDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository(ApplicaionDbContext db)
+        {
+            _db = db;
+            this.dbSet=_db.Set<T>();    
+        }
+        public void Add(T item)
+        {
+            dbSet.Add(item);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet.Where(filter);
+
+            return query.FirstOrDefault();
+        }
+
+        public IEnumerable<T> GetAll()
+        {
+            IQueryable<T> query = dbSet;
+
+            return query.ToList();
+        }
+
+        public void Remove(T item)
+        {
+            dbSet.Remove(item);
+        }
+
+        public void RemoveRange(IEnumerable<T> item)
+        {
+           dbSet.RemoveRange(item);
+        }
+    }
+}
