a5d7e0b1c3cf4e80a9ce53adae5e92fc24699672
nour hany <hananhanyfathy@gmail.com>
2025-02-16 22:13:30 +0200

    Implement ICategoryRepository

---
+++ Luky.DataAccess/Repository/IRepository/ICategoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using Luky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Luky.DataAccess.Repository.IRepository
+{
+    public interface ICategoryRepository:IRepository<Category>
+    {
+        void Update(Category obj);
+        void save();
+    }
+}
