d653f936f7e97c8e5fb24dcbf01f0aec8a57cb0e
nour hany <hananhanyfathy@gmail.com>
2025-02-22 17:09:58 +0200

    Add UnitOfWork

---
+++ ByteReader.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,27 @@
+﻿using ByteReader.DataAccess.Data;
+using ByteReader.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteReader.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicaionDbContext _db;
+
+        public UnitOfWork(ApplicaionDbContext db) 
+        {
+            _db = db;
+            Category = new CategoryRepository(_db);
+        }
+        public ICategoryRepository Category { get; private set; }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
