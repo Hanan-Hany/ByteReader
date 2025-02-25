73d271902c55b1a5a5d42e628345856f9bfce5e8
nour hany <hananhanyfathy@gmail.com>
2025-02-25 03:56:50 +0200

    Modify Add ProductRepository in UnitOfWork

--- ByteReader.DataAccess/Repository/IRepository/IUnitOfWork.cs
+++ ByteReader.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -8,6 +8,7 @@ namespace ByteReader.DataAccess.Repository.IRepository
 {
     public interface IUnitOfWork
     { 
+        IProductRepository Product { get; }
         ICategoryRepository Category { get; }
         void Save();
     }
