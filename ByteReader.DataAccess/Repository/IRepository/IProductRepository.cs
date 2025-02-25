2d8ef79f6cdd03e88071c5ae9109d0e2de00a98b
nour hany <hananhanyfathy@gmail.com>
2025-02-25 03:55:08 +0200

    Add Product Repository

---
+++ ByteReader.DataAccess/Repository/IRepository/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using ByteReader.Models;
+using ByteReader.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteReader.DataAccess.Repository.IRepository
+{
+    public interface IProductRepository:IRepository<Product>
+    {
+        void Update(Product obj);
+    }
+}
---
+++ ByteReader.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,26 @@
+﻿using ByteReader.DataAccess.Data;
+using ByteReader.DataAccess.Repository.IRepository;
+using ByteReader.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ByteReader.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>,IProductRepository
+    {
+        private readonly ApplicaionDbContext _db;
+
+        public ProductRepository(ApplicaionDbContext db) : base(db)
+        {
+            _db = db;
+
+        }
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
