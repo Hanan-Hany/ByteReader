60612a2bf90bf2077cdac17a164312be7afecf4f
nour hany <hananhanyfathy@gmail.com>
2025-03-05 20:51:26 +0200

    Add Product Controller

---
+++ ByteReader/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,116 @@
+﻿
+using ByteReader.Models;
+
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using ByteReader.DataAccess.Repository.IRepository;
+using ByteReader.Models.Models;
+
+
+namespace Luky_web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+
+        }
+
+
+        public IActionResult Index()
+        {
+
+            List<Product> products = _unitOfWork.Product.GetAll().ToList();
+
+            return View(products);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (product.Title == product.ISBN.ToString())
+            {
+                ModelState.AddModelError("Title", "The Title Cannot exactly match the ISBN");
+            }
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Created successfuly";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? product = _unitOfWork.Product.Get(u => u.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Product product)
+        {
+
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(product);
+                _unitOfWork.Save();
+                TempData["success"] = "Product Updated Successfuly";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Product? product = _unitOfWork.Product.Get(u => u.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePost(int? id)
+        {
+            Product? product = _unitOfWork.Product.Get(u => u.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+
+            _unitOfWork.Product.Remove(product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product Deleted Successfuly";
+            return RedirectToAction("Index");
+
+
+        }
+
+    }
+}
