7c49b5dd357e5cef6dddb963e8f2f20983213ce1
nour hany <hananhanyfathy@gmail.com>
2025-06-24 20:48:59 +0300

    Update Product model with new properties and attributes

--- ByteReader.Models/Models/Product.cs
+++ ByteReader.Models/Models/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
@@ -39,14 +40,15 @@ namespace ByteReader.Models.Models
         [Display(Name = "Price for 100+")]
         [Range(1, 1000)]
         public double Price100 { get; set; }
-
+       
         public int CategoryId { get; set; }
         [ForeignKey("CategoryId")]
- 
+        [ValidateNever]
         public Category category { get; set; }
 
-
-
+        [ValidateNever]
+        [Display(Name = "Image")]
+        public string ImageUrl { get; set; }
 
 
 
