4c80ba589afc3e9e4e11b6df507c3728150bf76f
nour hany <hananhanyfathy@gmail.com>
2025-06-24 20:46:39 +0300

    Add ProductVM class

---
+++ ByteReader.Models/ViewModel/ProductVM.cs
@@ -0,0 +1,19 @@
+﻿
+using ByteReader.Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ByteReader.Models.ViewModel
+{
+    public class ProductVM
+    {
+        public Product product { get; set; }
+        [ValidateNever]
+       public IEnumerable<SelectListItem> CategoryList { get; set; }
+    }
+}
