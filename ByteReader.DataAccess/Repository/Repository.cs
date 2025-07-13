0a625fcbefd6d15cb2d1ddef24410faed88c63de
nour hany <hananhanyfathy@gmail.com>
2025-07-13 19:58:54 +0300

    update IRepository & Repository to support eager loading includes

--- ByteReader.DataAccess/Repository/Repository.cs
+++ ByteReader.DataAccess/Repository/Repository.cs
@@ -13,33 +13,46 @@ namespace ByteReader.DataAccess.Repository
     public class Repository<T> : IRepository<T> where T : class
     {
         private readonly ApplicaionDbContext _db;
-        internal DbSet<T> dbSet;
+        internal DbSet<T> dbSet;   
         public Repository(ApplicaionDbContext db)
         {
             _db = db;
-            this.dbSet=_db.Set<T>();    
+            this.dbSet=_db.Set<T>();
+            _db.Products.Include(u => u.category).Include(u => u.CategoryId);
         }
         public void Add(T item)
         {
             dbSet.Add(item);
         }
 
-        public T Get(Expression<Func<T, bool>> filter)
+        public T Get(Expression<Func<T, bool>> filter , string? includeProperties = null)
         {
             IQueryable<T> query = dbSet.Where(filter);
-
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
             return query.FirstOrDefault();
         }
 
-        public IEnumerable<T> GetAll()
+        public IEnumerable<T> GetAll(string? includeProperties = null)
         {
             IQueryable<T> query = dbSet;
-
+            if (!string.IsNullOrEmpty(includeProperties))
+            {
+                foreach (var includeProp in includeProperties.Split(new char[] { ',' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    query = query.Include(includeProp);
+                }
+            }
             return query.ToList();
         }
 
         public void Remove(T item)
-        {
+        { 
             dbSet.Remove(item);
         }
 
